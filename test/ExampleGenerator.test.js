0985b32351444699d69083afb7bd45b01082b33e
leandrogilcarrano <leandro.gil@mulesoft.com>
2023-11-27 16:02:06 -0300

    fix(w-14278539): update unit test

--- test/ExampleGenerator.test.js
+++ test/ExampleGenerator.test.js
@@ -2321,7 +2321,7 @@ describe('ExampleGenerator', () => {
           schema = element._resolve(schema);
           const result = element._computeJsonPropertyValue(schema);
           assert.typeOf(result, 'object');
-          assert.deepEqual(result, { messages: [{ "referrers": "", "sequenceId": 1, "text": "", "type": "init", "tz": "", "variables": "" }]})
+          assert.deepEqual(result, { messages: [{ "referrers": [{"type": "Salesforce:Core:Bot:Id", "value": ""}], "sequenceId": 1, "text": "", "type": "init", "tz": "", "variables": [] }]})
         });
       });
     });
